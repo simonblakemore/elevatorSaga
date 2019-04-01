4671e5cbae7e638c7c1dc2a7530b03bc76dc644b
Simon <simonblakemore@ymail.com>
2019-04-01 13:51:05 +0100

    Update code.js

--- code.js
+++ code.js
@@ -3,40 +3,47 @@
         function ElevatorHelper(elevator, floors) {
             this.elevator = elevator;
             this.floors = floors;
-            
+
         }
-        
+
         ElevatorHelper.prototype.getNextFloor = function () {
             console.log(elevator);
             var currentFloor = this.elevator.currentFloor();
             for (var i = 0, len = floors.length; i < len; i++) {
-                
+
             }
         }
-        
+
         return new ElevatorHelper(elevator, floors);
     },
-    
-    init: function(elevators, floors) {
-        var _this = this,
-            elevator = elevators[0]; // Let's use the first elevator
-        
-        console.log(this.elevatorHelper);
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function() {
-            // let's go to all the floors (or did we forget one?)
-            //_this.elevatorHelper(elevator, floors).getNextFloor();
-            
-        });
-        
-        elevator.on("floor_button_pressed", function(floorNum) {
-            // Maybe tell the elevator to go to that floor?
-            elevator.goToFloor(floorNum);
-        });
-        
-    },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
-}
 
+        init: function(elevators, floors) {
+            var _this = this;
+            
+            elevators.forEach(function(elevator, index) {
+                elevator.on("floor_button_pressed", function(floorNum) {
+                    elevator.goToFloor(floorNum);
+                });
+                elevator.on('idle', function () {
+                    elevator.goToFloor(0);
+                    elevator.goToFloor(index);
+                });
+            });
+            
+            for(let i = 0, len = floors.length; i < len; i++) {
+                //floors[i].on('up_button_pressed', onFloorButtonPressed(i));
+                //floors[i].on('down_button_pressed', onFloorButtonPressed(i));
+            }
+            
+            function onFloorButtonPressed(floorNum) {
+                return function() {
+                    var index = Math.round(Math.random());
+                    elevators[index].goToFloor(floorNum)
+                }
+            }
+
+        },
+            update: function(dt, elevators, floors) {
+                // We normally don't need to do anything here
+            }
+}
